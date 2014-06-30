bb66464ddafa3c162dc8ced01724e826c0073a41
Kevin Snow <krsnow77@zoho.com>
2014-06-30 17:28:19 -0600

    Is It a Number?
    I put in the code to make a check to see if the string was parse-able

--- DigitalRoot/DigitalRoot/Program.cs
+++ DigitalRoot/DigitalRoot/Program.cs
@@ -16,35 +16,41 @@ namespace DigitalRoot
         }
         static void DigitalRoot(string rootThis)
         {
-            do {
+             int number = 0;
+             bool isANumber;
+            do
+            {
 
                 Console.WriteLine("Please enter any positive integer: ");
                 Console.WriteLine();
                 rootThis = Console.ReadLine();
+                
+                 isANumber = int.TryParse(rootThis, out number);
                 Console.WriteLine();
                 int total = 0;
-                
-                for (int i = 0; i < rootThis.Length; i++)
-                {
-                    int number = int.Parse(rootThis[i].ToString());
-                     total += number;
-                }
-                while (total.ToString().Length > 1)
-                {
-                    for (int i = 0; i < total.ToString().Length; i++)
+               
+
+                    for (int i = 0; i < rootThis.Length; i++)
                     {
-                        total =  int.Parse(total.ToString()[i].ToString());
+                        int number2 = int.Parse(rootThis[i].ToString());
+                        total += number;
                     }
-                }
+                    while (total.ToString().Length > 1)
+                    {
+                        for (int i = 0; i < total.ToString().Length; i++)
+                        {
+                            total = int.Parse(total.ToString()[i].ToString());
+                        }
+                    }
 
-                Console.WriteLine("Input: " + rootThis);
-                Console.WriteLine("Output: " + total);
-                Console.WriteLine();
+                    Console.WriteLine("Input: " + rootThis);
+                    Console.WriteLine("Output: " + total);
+                    Console.WriteLine();
 
+                
 
-
-            }while("0123456789".Contains(rootThis) && !rootThis.Contains(".") || !rootThis.Contains("-"));
-
+           } while (isANumber == true);
+            
 
         }
     }
