1799fddc6504e11da9ab9065a6cc4f9fb7c28af3
Kevin Snow <krsnow77@zoho.com>
2014-06-30 23:18:49 -0600

    formatting validator changes
    format validator changes

--- DigitalRoot/DigitalRoot/Program.cs
+++ DigitalRoot/DigitalRoot/Program.cs
@@ -16,40 +16,46 @@ namespace DigitalRoot
         }
         static void DigitalRoot(string rootThis)
         {
-             int number = 0;
-             bool isANumber;
-            do
-            {
+             
+             
+            
 
-                Console.WriteLine("Please enter any positive integer: ");
+                Console.WriteLine("Please enter any positive integer up to 17 digits: ");
                 Console.WriteLine();
                 rootThis = Console.ReadLine();
-                
-                 isANumber = int.TryParse(rootThis, out number);
+                long number = 0;
+                bool isANumber = long.TryParse(rootThis, out number);
                 Console.WriteLine();
-                int total = 0;
+            if(isANumber == true && rootThis.Length < 18 && !rootThis.Contains("-."))
+            {
+                long total = 0;
                
 
                     for (int i = 0; i < rootThis.Length; i++)
                     {
-                        int number2 = int.Parse(rootThis[i].ToString());
+                        long number2 = long.Parse(rootThis[i].ToString());
                         total += number;
                     }
                     while (total.ToString().Length > 1)
                     {
                         for (int i = 0; i < total.ToString().Length; i++)
                         {
-                            total = int.Parse(total.ToString()[i].ToString());
+                            total = long.Parse(total.ToString()[i].ToString());
                         }
                     }
 
                     Console.WriteLine("Input: " + rootThis);
                     Console.WriteLine("Output: " + total);
                     Console.WriteLine();
-
+                    DigitalRoot("");
+            }
+            else
+            {
+                DigitalRoot(""); 
+            }
                 
 
-           } while (isANumber == true);
+           
             
 
         }
